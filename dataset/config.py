aa07f113e1fd17f9d559d177ca6c75d166e8a5a0
mymayday <1157840315@qq.com>
2019-03-27 16:38:49 +0800

    config.py

---
+++ dataset/config.py
@@ -0,0 +1,20 @@
+from torch.cuda import is_available
+from easydict import EasyDict                 #Access dict values as attributes (works recursively)
+
+configer = EasyDict()
+
+configer.datasetpath="/datasets/ECUST2019"
+# configer.logspath   =
+# configer.modelspath =
+
+configer.facesize       = (64, 64)
+configer.n_channels     = 46
+configer.n_classes      = 40
+
+configer.modelname = "MobilefaceNet"
+
+configer.learningrate  = 1e-4
+configer.batchsize     = 64
+configer.n_epoch       = 300
+
+configer.cuda = is_available()
